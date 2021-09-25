ad596af0335c391a9583808c78e1a9061eb543aa
NNilawan <nilawan_l@cmu.ac.th>
2021-09-26 05:07:39 +0700

    Q3 3.1

--- src/services/OrganizerService.js
+++ src/services/OrganizerService.js
@@ -1,15 +1,34 @@
 import axios from 'axios'
 
 const apiClient = axios.create({
-  baseURL: process.env.VUE_APP_BACKEND_URL,
-  withCredentials: false,
-  headers: {
-    Accept: 'application/json',
-    'Content-Type': 'application/json'
-  }
+    baseURL: process.env.VUE_APP_BACKEND_URL,
+    withCredentials: false,
+    headers: {
+        Accept: 'application/json',
+        'Content-Type': 'application/json'
+    }
 })
 export default {
-  getOrganizers() {
-    return apiClient.get('/organizers')
-  }
+    getOrganizers() {
+        return apiClient.get('/organizers')
+    },
+    getOrganizerList(perPage, page) {
+        return apiClient.get('/organizerList?_limit=' + perPage + '&_page=' + page)
+    },
+    //Added new call
+    getOrganizer(id) {
+        return apiClient.get('/organizers/' + id)
+    },
+    saveOrganizer(organizer) {
+        return apiClient.post('/organizers', organizer)
+    },
+    uploadFile(file) {
+        let formData = new FormData()
+        formData.append('file', file)
+        return apiClient.post('/uploadFile', formData, {
+            headers: {
+                'Content-Type': 'multipart/form-data'
+            }
+        })
+    }
 }
